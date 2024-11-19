e0a7db855d6ff28adca42e2e6c237f83b9923780
sahil <sahiillx02@gmail.com>
2024-11-19 19:29:11 +0530

    variable

---
+++ src/basic/04_variable.py
@@ -0,0 +1,21 @@
+# in python a variable is a named location in memory that is used to store data.
+# python doesn't require you to declare the datatype of a variable when you create it.
+# python is dynamically typed.
+
+x = 10  # Integer
+name = "Sahil"  # String
+pi = 3.14159  # Float
+is_valid = True  # Boolean
+
+# variable's type can change during execution
+x = 10  # x is an integer
+x = "Hello"  # Now x is a string
+
+#  assign values to multiple variables in a single line
+a, b, c = 1, 2, 3  # a = 1, b = 2, c = 3
+x = y = z = 0  # All variables are set to 0
+
+# python doesn’t have a built-in constant type
+# variables that should not change are written in uppercase
+PI = 3.14159
+MAX_USERS = 100
