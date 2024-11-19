361183eeec4892244e6315fea74a80133e0ee5bb
sahil <sahiillx02@gmail.com>
2024-11-19 16:21:15 +0530

    Initial commit

---
+++ src/basic/01_module_pip.py
@@ -0,0 +1,22 @@
+# MODULE
+# in python, a module is a file
+# containing python code (functions, classes, or variables)
+# that can be imported and reused in other programs.
+
+# module is like a code library
+# written by somebody else.
+
+# two types of modules in python:
+# built in modules - these modules are ready to import and use and ships with the python interpreter. there is no need to install such modules explicitly.
+# external modules - these modules are imported from a third party file or can be installed using a package manager like pip or conda. since this code is written by someone else, we can install different versions of a same module with time.
+
+# PIP
+# pip stands for "Pip Installs Packages"
+# and is the default package manager for Python.
+# it allows you to install, upgrade, and manage
+# third-party python libraries from the Python Package Index (PyPI) or other repositories.
+
+# import pandas  # This is an example of external module
+# import hashlib  # This is an example of builtin module
+# pandas.read_csv("one.csv")
+# m = hashlib.sha256()
