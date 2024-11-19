f72c913be355085b335bbbc706c5decb03264cb7
sahil <sahiillx02@gmail.com>
2024-11-19 16:39:53 +0530

    first program

--- src/basic/02_program.py
+++ src/basic/02_program.py
@@ -1,3 +1,18 @@
-print("This is simple python program");
+# The print() function in Python is used to display output on the console.
+# It is one of the most commonly used functions for debugging and showing results.
 
-# print(This is simple python program); # error
+# print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
+
+# objects: One or more values you want to print.
+print("Hello, World!")
+print("Hello", "Sahil", "Welcome to Python")
+
+# sep: String inserted between values (default: space ' ').
+print("Python", "is", "fun", sep="-")  # Output: Python-is-fun
+
+# end: String appended after the last value (default: newline '\n').
+print("Hello", end=" ")
+print("World!")  # Output: Hello World!
+
+print("Hello")
+print("World!")  # by default, it uses '\n'
