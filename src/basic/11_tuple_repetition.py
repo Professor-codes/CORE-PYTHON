f0b1ac811dfe9ba29f4cc47f3a96fd1116201873
sahil <sahiillx02@gmail.com>
2024-11-20 11:03:40 +0530

    tuple repeatiton

---
+++ src/basic/11_tuple_repetition.py
@@ -0,0 +1,11 @@
+# repetition is implementing using the * operator
+# which repeats the elements of a tuple
+# into a specified number of times and creates a new tuple
+
+simple_tuple = (11, 20, 89)
+repeatedTuple = simple_tuple * 5
+print(repeatedTuple) # repeat the simple_tuple element with 5 times
+
+nested_tuple = (1, 2, (33, 44))
+repeatedTuple = nested_tuple * 2
+print(repeatedTuple) # repeat the nested_tuple element with 5 times
