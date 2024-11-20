c5d5086858531af0967d9e7b8d657846277d801a
sahil <sahiillx02@gmail.com>
2024-11-20 11:01:40 +0530

    tuple indexing

---
+++ src/basic/09_tuple_indexing.py
@@ -0,0 +1,11 @@
+# tuple indexing is the process of accessing elements of a tuple
+# using their index values
+# index starts from 0
+
+my_tuple = (10, 20, 30, 40, 50)
+# positive indexing
+print(my_tuple[0])  # Output: 10
+print(my_tuple[2])  # Output: 30
+# negative indexing
+print(my_tuple[-1])  # Output: 50
+print(my_tuple[-3])  # Output: 30
