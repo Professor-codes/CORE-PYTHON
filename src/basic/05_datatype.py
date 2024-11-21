64877f306ecd0df925f5774ddb16f059c65713fd
sahil <sahiillx02@gmail.com>
2024-11-21 10:53:43 +0530

    basic

--- src/basic/05_datatype.py
+++ src/basic/05_datatype.py
@@ -40,14 +40,56 @@ p = None
 print(type(p))  # Output: <class 'NoneType'>
 print(end="\n")
 
-# Type Conversion
-# Function	Converts to  	Example
-# int()	    Integer	        int(3.14) → 3
-# float()	Floating-point	float(10) → 10.0
-# str()	    String	        str(10) → "10"
-# list()	List	        list((1, 2, 3)) → [1, 2, 3]
-# tuple()	Tuple	        tuple([1, 2, 3]) → (1, 2, 3)
-# set()	    Set	            set([1, 2, 3]) → {1, 2, 3}
-# dict()	Dictionary	    dict([(1, "a"), (2, "b")])
+"""
+Type Conversion
+Function	Converts to  	Example
+int()	    Integer	        int(3.14) → 3
+float()	Floating-point	float(10) → 10.0
+str()	    String	        str(10) → "10"
+list()	List	        list((1, 2, 3)) → [1, 2, 3]
+tuple()	Tuple	        tuple([1, 2, 3]) → (1, 2, 3)
+set()	    Set	            set([1, 2, 3]) → {1, 2, 3}
+dict()	Dictionary	    dict([(1, "a"), (2, "b")])
+"""
+
+"""
+BASIC TYPES
+int - Represents integers
+float - Represents floating point numbers
+complex - Represents complex numbers
+bool - Represents boolean values
+NoneType - Represents null value
+
+SEQUENCE TYPES
+str - Represents string, immutable sequence of characters
+list [] - Represents mutable sequence of items
+tuple () - Represents immutable sequence of items
+range - Represents range of numbers
+
+SET TYPES
+set {} - unordered mutable collection of unique elements
+frozenset () - immutable version of set
+
+MAPPING TYPES
+dict {} - mutable collection of key-value pairs
+
+OTHER TYPES
+bytes b'text' - immutable sequence of bytes
+bytearray bytearray(b'text') - mutable sequence of bytes
+memoryview memoryview(b'text') - view of memory of an array or buffer
+
+USER DEFINED
+class Person:
+    def __init__(person, name):
+        person.name = name
+"""
+
+# class Food:
+#     def __init__(self, name):
+#         self.name = name
+#         print(f"{self.name}")
+# # instance of Food
+# food = Food("Panner Butter Masala + Naan")
+
 
 
